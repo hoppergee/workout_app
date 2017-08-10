e3478f94a33cd95393cbd5fc95854b8f2669b6c3
Ji Xiang <jx4554@gmail.com>
2017-08-11 07:49:58 +0800

    Implement create, show actions and show viwe template

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,11 +4,30 @@ class ExercisesController < ApplicationController
 
   end
 
+  def show
+    user = User.find(params[:user_id])
+    @exercise = user.exercises.find(params[:id])
+  end
+
   def new
     @exercise = current_user.exercises.new
   end
 
   def create
+    @exercise = current_user.exercises.new(exercise_params)
 
+    if @exercise.save
+      flash[:success] = "Exercise has been created"
+      redirect_to [current_user, @exercise]
+    else
+      flash[:alert] = "Exxercise has not been created"
+      render :new
+    end
   end
+
+  private
+
+    def exercise_params
+      params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+    end
 end
